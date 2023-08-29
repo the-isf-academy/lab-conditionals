4ca3c151b6b6ad93834484676c60f9136a9cee35
emma <eqbrown@isf.edu.hk>
2023-08-29 11:46:59 +0800

    slight code order change

--- user_input.py
+++ user_input.py
@@ -5,13 +5,17 @@ from turtle import *
 while True:
     
     drawing = input("What would you like me to draw? ")
-    size = int(input("How big should I draw it? "))
+
     if drawing == "square":
+        size = int(input("How big should I draw it? "))
+
         for i in range(4):
             forward(size)
             right(90)
+            
     elif drawing == "quit":
         break
+
     else:
         print("Sorry, I don't know how to draw that...")
 
